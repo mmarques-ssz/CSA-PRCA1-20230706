5e6b775a599fb71dfaf78101e2cd039a3f56a837
mmarques-ssz <37246763+mmarques-ssz@users.noreply.github.com>
2023-06-07 21:08:56 -0300

    Add files via upload

---
+++ func1/func1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace func1
+{
+    internal class Program
+    {
+
+        static void inverterTela()
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+        }
+
+        static int entrarNum(string msg, int li, int ls)
+        {
+            int x;
+            do
+            {
+                Console.Write(msg);
+                x = int.Parse(Console.ReadLine());
+            }
+            while (x < li || x > ls);
+            return x;
+        }
+        static int somar(int x, int y)
+        {
+            return x + y;
+        }
+
+        static void Main(string[] args)
+        {
+            int n1;
+            int n2;
+
+            inverterTela();
+
+            n1 = entrarNum("Digite o 1º numero: ", 0, 10);
+            n2 = entrarNum("Digite o 2º numero: ", 10, 20);
+            Console.WriteLine("Soma de {0} com {1} é {2}", 
+                n1, 
+                n2, 
+                somar(n1, n2));
+
+        }
+    }
+}
---
+++ func2/func2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace func2
+{
+    internal class Program
+    {
+        static int[] num;
+
+        static void inverterTela()
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+        }
+        static void entrar()
+        {
+            for(int i=0; i<5; ++i)
+            {
+                Console.Write("Digite o {0}º valor: ", i+1);
+                num[i] = int.Parse(Console.ReadLine());
+            }
+        }
+        static void dobrar()
+        {
+            for (int i = 0; i < 5; ++i)
+            {
+                num[i] *= 2;
+            }
+        }
+        static void mostrar()
+        {
+            for (int i = 0; i < 5; ++i)
+            {
+                Console.WriteLine("{0}º valor: {1}", i+1, num[i]);
+            }
+            Console.WriteLine("-----------");
+        }
+        static void Main(string[] args)
+        {
+            num = new int[5];
+            inverterTela();
+            entrar();
+            mostrar();
+
+            dobrar();
+            mostrar();
+        }
+    }
+}
